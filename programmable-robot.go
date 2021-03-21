133cf13cb1cf70f3d8273d924c873a041a649f92
caishunfeng <caishunfeng@guorou.tv>
2021-03-21 12:42:05 +0800

    update

--- programmable-robot.go
+++ programmable-robot.go
@@ -39,47 +39,31 @@ import "fmt"
 // 链接：https://leetcode-cn.com/problems/programmable-robot
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 func main() {
-	fmt.Println(robot("URR", [][]int{}, 3, 2))
-	fmt.Println(robot("URR", [][]int{{2, 2}}, 3, 2))
-	fmt.Println(robot("URR", [][]int{{4, 2}}, 3, 2))
-	fmt.Println(robot("URR", [][]int{}, 0, 0))
+	// fmt.Println(robot("URR", [][]int{}, 3, 2))
+	// fmt.Println(robot("URR", [][]int{{2, 2}}, 3, 2))
+	// fmt.Println(robot("URR", [][]int{{4, 2}}, 3, 2))
+	// fmt.Println(robot("RRU", [][]int{{5, 5}, {9, 4}, {9, 7}, {6, 4}, {7, 0}, {9, 5}, {10, 7}, {1, 1}, {7, 5}}, 1486, 743))
+	// fmt.Println(robot("URRUU", [][]int{{6, 0}, {3, 4}, {5, 2}, {3, 0}}, 6, 6))
+	// fmt.Println(robot("RUURRR", [][]int{{9, 5}, {4, 4}, {0, 10}, {3, 9}}, 80, 40))
+	fmt.Println(robot("UUUURR", [][]int{{0, 8}, {1, 2}, {7, 3}, {1, 5}, {7, 9}, {4, 9}, {3, 3}}, 990, 1890))
 }
 
 func robot(command string, obstacles [][]int, x int, y int) bool {
-	// if len(command) < 2 || len(command) > 1000 {
-	// 	return false
-	// }
-
-	// if !strings.Contains(command, "U") || !strings.Contains(command, "R") {
-	// 	return false
-	// }
-
-	// if x < 0 || x > 1e9 {
-	// 	return false
-	// }
-
-	// if y < 0 || y > 1e9 {
-	// 	return false
-	// }
-
-	// if len(obstacles) > 1000 {
-	// 	return false
-	// }
-	// var sites [][]int
-
-	// fmt.Println(obstacles)
-	// for i := 0; i < len(obstacles); i++ {
-	// 	for j := 0; j < len(obstacles[i]); j++ {
-	// 		fmt.Println(obstacles[i][j])
-	// 	}
-	// }
-
 	baseX := 0
 	baseY := 0
 
+	for i := 0; i < len(obstacles)-1; i++ {
+		for j := i + 1; j < len(obstacles); j++ {
+			if obstacles[i][0] > obstacles[j][0] {
+				obstacles[i], obstacles[j] = obstacles[j], obstacles[i]
+			} else if obstacles[i][0] == obstacles[j][0] && obstacles[i][1] > obstacles[j][1] {
+				obstacles[i], obstacles[j] = obstacles[j], obstacles[i]
+			}
+		}
+	}
+
 	for true {
 		for i := 0; i < len(command); i++ {
-			// fmt.Println(baseX, baseY)
 			switch command[i] {
 			case 'U':
 				baseY += 1
@@ -90,10 +74,8 @@ func robot(command string, obstacles [][]int, x int, y int) bool {
 			default:
 				return false
 			}
-			for k := 0; k < len(obstacles); k++ {
-				if obstacles[k][0] == baseX && obstacles[k][1] == baseY {
-					return false
-				}
+			if isMeet(obstacles, baseX, baseY) {
+				return false
 			}
 			if baseX == x && baseY == y {
 				return true
@@ -104,4 +86,30 @@ func robot(command string, obstacles [][]int, x int, y int) bool {
 		}
 	}
 	return false
+}
+
+func isMeet(obstacles [][]int, baseX int, baseY int) bool {
+	l := len(obstacles)
+	if l == 0 {
+		return false
+	}
+	if l == 1 {
+		if obstacles[l-1][0] == baseX && obstacles[l-1][1] == baseY {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	mid := 0
+	if l%2 == 0 {
+		mid = l / 2
+	} else {
+		mid = l/2 + 1
+	}
+	if obstacles[mid-1][0] < baseX || obstacles[mid-1][1] < baseY {
+		return isMeet(obstacles[mid:], baseX, baseY)
+	} else {
+		return isMeet(obstacles[:mid], baseX, baseY)
+	}
 }
